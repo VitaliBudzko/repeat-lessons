b307dc8d5b3c9946b4500b8094fac272e32a7b9a
Vitali Budzko <labanka1983@gmail.com>
2018-04-19 21:03:51 +0300

    The Table View Controller and the MealTableViewCell.swift file have been created.

---
+++ FootTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FootTracker
+//
+//  Created by Sasha Dubikovskaya on 19.04.2018.
+//  Copyright © 2018 repeat lessons. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
