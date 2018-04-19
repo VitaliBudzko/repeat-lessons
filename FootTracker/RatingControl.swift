0f4902a644ee1d33ac1cda5e020be8509cb2ce5a
Vitali Budzko <labanka1983@gmail.com>
2018-04-19 16:21:54 +0300

    The button under the image view has been divide into five buttons.

--- FootTracker/RatingControl.swift
+++ FootTracker/RatingControl.swift
@@ -8,8 +8,23 @@
 
 import UIKit
 
-class RatingControl: UIStackView {
-
+@IBDesignable class RatingControl: UIStackView {
+    //MARK: Properties
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
+        didSet {
+            setupButtons()
+        }
+    }
+    
+    @IBInspectable var starCount: Int = 5 {
+        didSet {
+            setupButtons()
+        }
+    }
+    
+    private var ratingButtons = [UIButton]()
+    
+    var rating = 0
   //MARK: Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -29,24 +44,29 @@ class RatingControl: UIStackView {
     //MARK: Private Methods
     private func setupButtons() {
         
-        // Create the button
-        let button = UIButton()
-        button.backgroundColor = UIColor.red
-        
-        // Add constraints
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
-        
-        // Setup the button action
-        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
-        
-        // Add the button to the stack
-        addArrangedSubview(button)
-    }
+        for _ in 0..<starCount {
+            // Create the button
+            let button = UIButton()
+            button.backgroundColor = UIColor.red
+            
+            // Add constraints
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
+            
+            // Setup the button action
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            
+            // Add the button to the stack
+            addArrangedSubview(button)
+            
+            // Add the new button to the rating button array
+            ratingButtons.append(button)
+            
+        }
     
 }
-
+}
 
 
 
